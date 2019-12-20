4db140c140bc416266c58e33fde3f1747e1b2480
grayson <grayson@returntocorp.com>
2019-12-20 17:18:55 -0500

    Added tests for  case.

--- tests/test_option_param_name_checker.py
+++ tests/test_option_param_name_checker.py
@@ -66,3 +66,59 @@ def test_bool_option_with_slash():
           def build(shout): pass
         """
     )
+
+
+def test_expose_value_false():
+    code = """
+@click.command("run", short_help="Run a development server.")
+@click.option("--host", "-h", default="127.0.0.1", help="The interface to bind to.")
+@click.option("--port", "-p", default=5000, help="The port to bind to.")
+@click.option(
+    "--cert", type=CertParamType(), help="Specify a certificate file to use HTTPS."
+)
+@click.option(
+    "--key",
+    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+    callback=_validate_key,
+    expose_value=False,
+    help="The key file to use when specifying a certificate.",
+)
+@click.option(
+    "--reload/--no-reload",
+    default=None,
+    help="Enable or disable the reloader. By default the reloader "
+    "is active if debug is enabled.",
+)
+@click.option(
+    "--debugger/--no-debugger",
+    default=None,
+    help="Enable or disable the debugger. By default the debugger "
+    "is active if debug is enabled.",
+)
+@click.option(
+    "--eager-loading/--lazy-loading",
+    default=None,
+    help="Enable or disable eager loading. By default eager "
+    "loading is enabled if the reloader is disabled.",
+)
+@click.option(
+    "--with-threads/--without-threads",
+    default=True,
+    help="Enable or disable multithreading.",
+)
+@click.option(
+    "--extra-files",
+    default=None,
+    type=SeparatedPathType(),
+    help=(
+        "Extra files that trigger a reload on change. Multiple paths"
+        " are separated by '{}'.".format(os.path.pathsep)
+    ),
+)
+@pass_script_info
+def run_command(
+    info, host, port, reload, debugger, eager_loading, with_threads, cert, extra_files
+):
+    debug = get_debug_flag()
+"""
+    assert len(check_code(code)) == 0
